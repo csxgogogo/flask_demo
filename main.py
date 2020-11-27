f04bd19386520c8ef3efa5565ed69b040e8f2130
chen <chenshaoxiong@tungee.com>
2020-11-27 17:25:49 +0800

    add env and requirments

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
-
 app = Flask(__name__)
 
-@app.route("/",methods=["GET"])
+
+@app.route("/", methods=["GET"])
 def index():
-    return {"msg":"welcome chen page"}
+    return {"msg": "welcome chen page"}
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=9999,debug=True)
+    app.run(host="0.0.0.0", port=9999, debug=True)
