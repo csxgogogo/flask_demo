7b58f89a69bff66d3bc269f7f0fd36fb5d818539
chen <chenshaoxiong@tungee.com>
2020-11-27 17:55:47 +0800

    test gevent run server

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 from flask import Flask
+from gevent import monkey, pywsgi
 
 app = Flask(__name__)
+monkey.patch_all()
 
 
 @app.route("/", methods=["GET"])
 def index():
     return {"msg": "welcome chen page"}
 
+
+http_server = pywsgi.WSGIServer(('localhost', 8888), app)
+
+app.debug = True
+if app.debug:
+    from werkzeug.serving import run_with_reloader
+
+
+    @run_with_reloader
+    def run_server():
+        http_server.serve_forever()
+else:
+    http_server.serve_forever()
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=9999, debug=True)
